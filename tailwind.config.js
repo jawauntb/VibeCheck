77734ea396772adec0c983a573a84c4dc94870a2
jawaun <jawaun.brown95@gmail.com>
2023-04-30 13:47:04 -0400

    update tailwinds

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,51 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        cycling: 'cycling 4s linear infinite',
+      },
+      keyframes: {
+        cycling: {
+          '0%': { content: '"😎"' },
+          '25%': { content: '"🔥"' },
+          '50%': { content: '"🥶"' },
+          '75%': { content: '"🪩"' },
+          '100%': { content: '"🌴"' },
+        },
+      },
+      boxShadow: {
+        '2': '0 2px 4px rgba(0, 0, 0, 0.1)',
+      },
     },
   },
   plugins: [],
 }
+
+
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      animation: {
+        cycling: 'cycling 4s linear infinite',
+      },
+      keyframes: {
+        cycling: {
+          '0%': { content: '"😎"' },
+          '25%': { content: '"🔥"' },
+          '50%': { content: '"🥶"' },
+          '75%': { content: '"🪩"' },
+          '100%': { content: '"🌴"' },
+        },
+      },
+      boxShadow: {
+        '2': '0 2px 4px rgba(0, 0, 0, 0.1)',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
